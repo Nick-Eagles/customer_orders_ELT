5563e2aae4bb7d5fba0ae15cbec06c7699f898a1
Nick-Eagles <nickeagles77@gmail.com>
2025-11-26 20:58:14 -0500

    Again adjust paths for compatibility with DBeaver

--- scripts/bronze/02_bulk_insert.sql
+++ scripts/bronze/02_bulk_insert.sql
@@ -3,7 +3,7 @@ Bulk insert data into Bronze-layer tables from CSV files
 
 After 01_init_tables.sql, this script truncates any existing data, then loads
 fresh data from the source CSVs (using absolute paths on the local filesystem
-in this case).
+in this case for compatibility with DBeaver).
 */
 
 -- #############################################################################
@@ -12,18 +12,18 @@ in this case).
 
 TRUNCATE TABLE bronze.crm_cust_info;
 COPY bronze.crm_cust_info
-FROM '/home/nick/Documents/programming/customer_orders_ELT/data/source_CRM/cust_info.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', FREEZE true);
+FROM '/var/lib/postgresql/imports/cust_info.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',');
 
 TRUNCATE TABLE bronze.crm_prd_info;
 COPY bronze.crm_prd_info
-FROM '/home/nick/Documents/programming/customer_orders_ELT/data/source_CRM/prd_info.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', FREEZE true);
+FROM '/var/lib/postgresql/imports/prd_info.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',');
 
 TRUNCATE TABLE bronze.crm_sales_details;
 COPY bronze.crm_sales_details
-FROM '/home/nick/Documents/programming/customer_orders_ELT/data/source_CRM/sales_details.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', FREEZE true);
+FROM '/var/lib/postgresql/imports/sales_details.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',');
 
 -- #############################################################################
 -- #  Load ERP source data
@@ -31,15 +31,15 @@ WITH (FORMAT csv, HEADER true, DELIMITER ',', FREEZE true);
 
 TRUNCATE TABLE bronze.erp_cust_az12;
 COPY bronze.erp_cust_az12
-FROM '/home/nick/Documents/programming/customer_orders_ELT/data/source_ERP/CUST_AZ12.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', FREEZE true);
+FROM '/var/lib/postgresql/imports/CUST_AZ12.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',');
 
 TRUNCATE TABLE bronze.erp_loc_a101;
 COPY bronze.erp_loc_a101
-FROM '/home/nick/Documents/programming/customer_orders_ELT/data/source_ERP/LOC_A101.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', FREEZE true);
+FROM '/var/lib/postgresql/imports/LOC_A101.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',');
 
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 COPY bronze.erp_px_cat_g1v2
-FROM '/home/nick/Documents/programming/customer_orders_ELT/data/source_ERP/PX_CAT_G1V2.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', FREEZE true);
+FROM '/var/lib/postgresql/imports/PX_CAT_G1V2.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',');
