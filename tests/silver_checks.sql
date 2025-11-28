4acb16d1de170b6c72937c938f05c901d3a603d8
Nick-Eagles <nickeagles77@gmail.com>
2025-11-28 17:42:32 -0500

    Add tests for the second silver table

--- tests/silver_checks.sql
+++ tests/silver_checks.sql
@@ -1,3 +1,11 @@
+-- #############################################################################
+-- #   CRM tables
+-- #############################################################################
+
+-- #----------------------------------------------------------------------------
+-- #   silver.crm_cust_info
+-- #----------------------------------------------------------------------------
+
 -- Primary key should neither be null nor duplicated
 --
 -- Expectation: 0 rows returned
@@ -14,3 +22,27 @@ HAVING cst_id IS NULL OR COUNT(*) > 1;
 SELECT * FROM silver.crm_cust_info
 WHERE cst_marital_status NOT IN ('Married', 'Single')
    OR cst_gndr NOT IN ('Male', 'Female');
+
+-- #----------------------------------------------------------------------------
+-- #   silver.crm_prd_info
+-- #----------------------------------------------------------------------------
+
+-- I did some complex manipulation of dates to get to silver; just check that
+-- end dates are valid here (not earlier than start dates)
+--
+-- Expectation: 0 rows returned
+SELECT prd_start_dt, prd_end_dt
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
+
+-- I technically allow, but don't expect product lines outside of this set
+--
+-- Expectation: 0 rows returned
+SELECT * FROM silver.crm_prd_info
+WHERE prd_line NOT IN ('Mountain', 'Road', 'Other sales', 'Touring');
+
+-- Costs should be non-negative
+--
+-- Expectation: 0 rows returned
+SELECT * FROM silver.crm_prd_info
+WHERE prd_cost < 0;
