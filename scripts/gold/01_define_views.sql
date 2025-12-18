913f37b7eba1ab751ef86f4feefcaf1e40244c94
Nick-Eagles <nickeagles77@gmail.com>
2025-12-18 15:09:26 -0500

    Produce the gold product dimension view

--- scripts/gold/01_define_views.sql
+++ scripts/gold/01_define_views.sql
@@ -10,7 +10,8 @@ a concern for the scale of this project.
 -- #############################################################################
 
 -- All customer info joined together; one row per customer
-CREATE OR REPLACE VIEW gold.dim_customer AS
+DROP VIEW IF EXISTS gold.dim_customer;
+CREATE VIEW gold.dim_customer AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY crm.cst_id) AS surrogate_key,
     -- Retain the 4 forms of customer keys seen in the raw data
@@ -35,3 +36,27 @@ LEFT JOIN silver.erp_cust_az12 AS erp_az
     ON crm.cst_id = erp_az.cid_clean
 LEFT JOIN silver.erp_loc_a101 AS erp_loc
     ON crm.cst_id = erp_loc.cid_clean;
+
+-- All current product info joined with ERP category info; one row per product
+DROP VIEW IF EXISTS gold.dim_product;
+CREATE VIEW gold.dim_product AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY crm.prd_key) AS surrogate_key,
+    -- Product IDs/ keys come in 3 forms; retain all of them
+    crm.prd_id AS crm_key1,
+    crm.prd_key AS crm_key2,
+    crm.prd_clean_key AS crm_key3,
+    -- Then put product info followed by category info
+    crm.prd_nm AS name,
+    crm.prd_cost AS cost,
+    crm.prd_line AS line,
+    crm.prd_start_dt AS start_date,
+    crm.prd_cat AS category_key,
+    erp.cat AS category_name,
+    erp.subcat AS subcategory_name,
+    erp.maintenance AS category_maintenance
+FROM silver.crm_prd_info AS crm
+LEFT JOIN silver.erp_px_cat_g1v2 AS erp
+    ON crm.prd_cat = erp.id
+-- For gold, only include products that are still active
+WHERE crm.prd_end_dt IS NULL;
