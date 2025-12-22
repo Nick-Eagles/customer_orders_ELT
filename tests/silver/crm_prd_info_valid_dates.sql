116604c36f06b718a62ab19f7995507c8cfc0107
Nick-Eagles <nickeagles77@gmail.com>
2025-12-22 16:10:22 -0500

    Add some documentation in various models and tests

--- tests/silver/crm_prd_info_valid_dates.sql
+++ tests/silver/crm_prd_info_valid_dates.sql
@@ -1,8 +1,6 @@
 -- Table: silver crm_prd_info
 --
 -- Test: end dates should always be later than or equal to start dates
-SELECT
-    prd_start_dt,
-    prd_end_dt
+SELECT prd_start_dt, prd_end_dt
 FROM {{ ref('crm_prd_info') }}
 WHERE prd_end_dt < prd_start_dt
